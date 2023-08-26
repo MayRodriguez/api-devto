93bb2e6523458f0e65ff2a508ceac39e1bec2a39
may <altamiranormay@gmail.com>
2023-08-26 15:56:55 -0700

    front + back, lets continue with login

--- js/modules/postMain.js
+++ js/modules/postMain.js
@@ -1,6 +1,6 @@
 const getPosts = async () => {
   let response = await fetch(
-    "http://localhost:3003/post"
+    "http://localhost:3001/post"
   );
   let data = await response.json();
   return data;

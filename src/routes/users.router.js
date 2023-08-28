4f10d0158673fe2b562a1b22fcc417c8ff5a6e2a
Hector Hernandez <132419045+HectorsTech@users.noreply.github.com>
2023-08-28 18:41:56 +0000

    post router created

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -4,6 +4,22 @@ const users = require("../usecases/user.usecase")
 const router = express.Router();
 
 
-router.post("/", (request, response) =>{
+router.post("/", async (request, response) =>{
+    try{
+
+        const userData = request.body;
+        const newUser = await users.create(userData);
+        
+        
+    }catch(error){
+        const status = error.name === "ValidationError" ? 400 : 500;
+        response.status(status);
+        response.json({
+        message: "something went wrong",
+        error: error.message,
+    });
+
+    }
+
 
 })

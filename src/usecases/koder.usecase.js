b291d33c0d226096a58cf0e954946354d597c4c8
Hector Hernandez <132419045+HectorsTech@users.noreply.github.com>
2023-08-28 18:32:36 +0000

    users.router created

--- src/usecases/koder.usecase.js
+++ src/usecases/koder.usecase.js
@@ -1,3 +1,30 @@
 const mongoose = require("mongoose")
 const ModelUser = require("../models/user.model")
 const createError = require("http-errors")
+const bcryptjs = require("../lib/bcrypt")
+const userModel = require("../models/user.model")
+
+//Post User, para crear un usuario 
+
+async function create(userData){
+    //Validamos si el usuraio existe con el mismo email
+    const existingUser = await userModel.findOne({ email: userData.email});
+    if (existingUser){
+        throw new createError (412, "email already registed")
+    }
+    
+    const passwordRegex = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-.+]).{8,}$")
+
+    if(!passwordRegex.test(userData.password)){
+        throw new createError(400, "Password too weak")
+    }
+
+    koderData.password = bcryptjs.encrypt(koderData.password)
+
+    const newUser = await userModel.create(koderData)
+    return newUser
+}
+
+module.exports = {
+    create
+}

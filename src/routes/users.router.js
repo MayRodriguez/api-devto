d809733867e3e75c44680917d247bc63b255e4e6
Hector Hernandez <132419045+HectorsTech@users.noreply.github.com>
2023-08-28 18:33:48 +0000

    names and routes changed

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,6 +1,9 @@
 const express = require("express")
-const users = require("../usecases/koder.usecase")
+const users = require("../usecases/user.usecase")
 
 const router = express.Router();
 
 
+router.post("/", (request, response) =>{
+
+})
